1eec47cc0642bd8f7092f9ce9ce8a19bdaf990d2
diffrent <julian_chernashki@abv.bg>
2016-12-17 23:41:41 +0200

    entityContact

---
+++ Blog/src/SoftUniBlogBundle/Entity/Contacts.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace SoftUniBlogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Contacts
+ *
+ * @ORM\Table(name="contacts")
+ * @ORM\Entity(repositoryClass="SoftUniBlogBundle\Repository\ContactsRepository")
+ */
+class Contacts
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text", nullable=true)
+     */
+    private $content;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return Contacts
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+}
+
