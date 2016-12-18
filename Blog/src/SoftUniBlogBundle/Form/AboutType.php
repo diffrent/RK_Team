7fde0e0f87b14d66b1aed4abd20874f20a0acf83
JosifP <plachkov.jp@gmail.com>
2016-12-19 00:07:56 +0200

    addAboutForm

---
+++ Blog/src/SoftUniBlogBundle/Form/AboutType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace SoftUniBlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AboutType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('content', TextType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+
+    }
+
+    public function getName()
+    {
+        return 'soft_uni_blog_bundle_about_type';
+    }
+}
