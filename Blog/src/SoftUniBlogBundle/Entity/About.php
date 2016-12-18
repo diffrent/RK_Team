327ea14c5f3a690dea8f8e84b2b62108376f64e5
JosifP <plachkov.jp@gmail.com>
2016-12-19 00:06:38 +0200

    addAboutEntity

---
+++ Blog/src/SoftUniBlogBundle/Entity/About.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace SoftUniBlogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * About
+ *
+ * @ORM\Table(name="about")
+ * @ORM\Entity(repositoryClass="SoftUniBlogBundle\Repository\AboutRepository")
+ */
+class About
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text", nullable=true)
+     */
+    private $content;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return About
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+}
+
